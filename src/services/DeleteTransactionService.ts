6326bd8bedc198b19e14d34abbb69935cac5f0ed
Leonardo Marcos <leonardo.marcos@cwi.com.br>
2020-04-23 03:05:09 -0300

    feat: DeleteTransactionService

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,27 @@
-// import AppError from '../errors/AppError';
+import { getRepository, TransactionRepository } from 'typeorm';
+import AppError from '../errors/AppError';
+import Transaction from '../models/Transaction';
 
+interface Request {
+  id: string;
+}
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute({ id }: Request): Promise<void> {
+    const transactionRepository = getRepository(Transaction);
+
+    const transactionExists = await transactionRepository.findOne({
+      where: {
+        id,
+      },
+    });
+
+    if (!transactionExists) {
+      throw new AppError('Transaction doesn`t exists.');
+    }
+
+    await transactionRepository.delete({
+      id: transactionExists.id,
+    });
   }
 }
 
