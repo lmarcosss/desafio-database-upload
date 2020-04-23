8eb22c58f7528c5e4ec0ac0e2e6ffe535d8d9c26
Leonardo Marcos <leonardo.marcos@cwi.com.br>
2020-04-23 03:05:00 -0300

    feat: CreateTransactionService

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,48 @@
-// import AppError from '../errors/AppError';
-
+import { getCustomRepository } from 'typeorm';
+import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
+import CategoryService from './CreateCategoryService';
+import AppError from '../errors/AppError';
+
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const transactionRepository = getCustomRepository(TransactionsRepository);
+
+    const { total } = await transactionRepository.getBalance();
+
+    if (type === 'outcome' && total < value) {
+      throw new AppError('The balance is not enough to this transaction.');
+    }
+
+    const categoryService = new CategoryService();
+
+    const newCategory = await categoryService.execute({ title: category });
+
+    const transaction = transactionRepository.create({
+      title,
+      value,
+      type,
+      category: {
+        id: newCategory.id,
+        title: newCategory.title,
+      },
+    });
+
+    await transactionRepository.save(transaction);
+
+    return transaction;
   }
 }
 
