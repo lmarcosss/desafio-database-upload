4950ceb96e9003b6a64dfc0e90366d196ae2e699
Leonardo Marcos <leonardo.marcos@cwi.com.br>
2020-04-23 03:04:07 -0300

    feat: create TransactionsRepository

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -8,10 +8,72 @@ interface Balance {
   total: number;
 }
 
+interface TransactionDTO {
+  transactions: Array<Transaction>;
+  balance: Balance;
+}
+
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
+  private balance: Balance;
+
+  constructor() {
+    super();
+
+    this.balance = {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    };
+  }
+
+  public async all(): Promise<TransactionDTO> {
+    const transactions = await this.createQueryBuilder('transactions')
+      .select('transactions.id')
+      .addSelect('transactions.title')
+      .addSelect('transactions.type')
+      .addSelect('transactions.value')
+      .addSelect('category.id')
+      .addSelect('category.title')
+      .leftJoin('transactions.category', 'category')
+      .getMany();
+
+    const balance = await this.getBalance();
+
+    return {
+      transactions,
+      balance,
+    };
+  }
+
+  private getIncomeValue(transactions: Array<Transaction>): number {
+    return transactions.reduce((total, transaction) => {
+      if (transaction.type === 'income') return total + transaction.value;
+
+      return total;
+    }, 0);
+  }
+
+  private getOutcomeValue(transactions: Array<Transaction>): number {
+    return transactions.reduce((total, transaction) => {
+      if (transaction.type === 'outcome') return total + transaction.value;
+
+      return total;
+    }, 0);
+  }
+
   public async getBalance(): Promise<Balance> {
-    // TODO
+    const transactions = await this.find();
+    const income = this.getIncomeValue(transactions);
+    const outcome = this.getOutcomeValue(transactions);
+
+    this.balance = {
+      income,
+      outcome,
+      total: income - outcome,
+    };
+
+    return this.balance;
   }
 }
 
